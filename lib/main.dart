a8eea5a4eed5ef051673496cc2632f61eb2411ed
Diego Minelli <diego.minelli@hotmail.com>
2022-06-16 16:45:06 -0300

    fazendo a refatoracao do codigo

--- lib/main.dart
+++ lib/main.dart
@@ -2,22 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
       home: Scaffold(
-        body: Column(
-          children: const <Widget>[
-            Card(
-                child: ListTile(
-              leading: Icon(Icons.monetization_on),
-              title: Text('100.0'),
-              subtitle: Text('1000'),
-            )),
-            Card(
-                child: ListTile(
-              leading: Icon(Icons.monetization_on),
-              title: Text('200.0'),
-              subtitle: Text('1000'),
-            )),
-          ],
-        ),
+        body: const ListaTransferencia(),
         appBar: AppBar(
           title: const Text('Transferências'),
         ),
@@ -27,3 +12,43 @@ void main() => runApp(MaterialApp(
         ),
       ),
     ));
+
+class ListaTransferencia extends StatelessWidget {
+  const ListaTransferencia({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        ItemTransferencia(Transferencia(100.0, 1000)),
+        ItemTransferencia(Transferencia(200.0, 1001)),
+        ItemTransferencia(Transferencia(300.0, 1100)),
+      ],
+    );
+  }
+}
+
+// ignore: use_key_in_widget_constructors
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia _transferencia;
+
+  // ignore: use_key_in_widget_constructors
+  const ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: ListTile(
+      leading: const Icon(Icons.monetization_on),
+      title: Text(_transferencia.valor.toString()),
+      subtitle: Text(_transferencia.numeroConta.toString()),
+    ));
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
+}
